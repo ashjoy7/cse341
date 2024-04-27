f47592cc9d7c06bfa81e6ae2122e755bec73d370
ashjoy7 <ashjoy7@gmail.com>
2024-04-27 01:05:13 -0700

    update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use('/', require('/.routes'));
+app.use('/', require('P:\Repositories\cse341\cse341\routes\index.js'));
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
