46a3069ffa8aef432102b3d77746de7cf7e3e24d
ashjoy7 <ashjoy7@gmail.com>
2024-04-27 02:32:31 -0700

    update

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,9 @@
 const awesomeFunction = (req, res, next) => {
-    res.json('Awesome person');
+    res.json('Angela Egan');
 };
 
 const returnanotherPerson = (req, res, next) => {
-    res.json('Supa awesome person');
+    res.json('Mark Egan');
 };
 
 module.exports = {awesomeFunction, returnanotherPerson};
