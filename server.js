4c69aa344341d34a9dce86fb0f6c0bfc63ad54ad
ashjoy7 <ashjoy7@gmail.com>
2024-04-27 00:49:34 -0700

    update routes folder

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 var express = require('express');
 var app = express();
 
-const routes = require('express').Router();
-
-routes.get('/', (req, res, next) => {
-    res.json('Awesome person');
-});
+app.use('/', require('/.routes'));
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
